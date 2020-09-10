3be210e17ee49765e796cd1f6e0133ab461ddd57
tanuj <tanujvyas10@gmail.com>
2020-09-10 23:18:13 +0530

    airbnbn logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home'
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-     <Home/>
+    <Header/> 
+    <Home/>
+     
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,8 @@ import './header.css'
 const index = () => {
     return (
         <div>
-            <h2>Header</h2>
+           
+        <img className= "logo" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png"/>
         </div>
     )
 }
