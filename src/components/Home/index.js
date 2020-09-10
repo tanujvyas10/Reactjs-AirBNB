18b2b3ed754d6f55b897948804ce45ed72e40c26
tanuj <tanujvyas10@gmail.com>
2020-09-10 23:09:02 +0530

    add homes

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './main.css'
+const index = () => {
+    return (
+        <div className = 'home'>
+            <h1>Home Components</h1>
+        </div>
+    )
+}
+
+export default index
