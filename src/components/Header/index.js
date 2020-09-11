e48206b0299e94f49e6816a5c58b947e6895697d
tanuj <tanujvyas10@gmail.com>
2020-09-11 13:50:30 +0530

    header styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import './header.css'
+import {Avatar} from '@material-ui/core'
+import TranslateIcon from '@material-ui/icons/Translate';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SearchIcon from '@material-ui/icons/Search';
 const index = () => {
     return (
-        <div>
+        <div className = "header">
            
         <img className= "logo" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png"/>
+       
+       <div className = "center">
+       <input type = "text"/>
+       <SearchIcon/>
+       </div>
+       <div className = "right">
+       <p>Become a host</p>
+       <TranslateIcon/>
+       <ExpandMoreIcon/>
+       <Avatar/>
+       </div>
         </div>
     )
 }
