746f10ef18a535e54b6751983cb70a9ffeb1a4c5
tanuj <tanujvyas10@gmail.com>
2020-09-11 17:04:26 +0530

    wideshow styling

--- src/components/Home/WideShow/index.js
+++ src/components/Home/WideShow/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './wideshow.css'
+import {Button} from '@material-ui/core'
 const index = () => {
     return (
         <div className = "wideshow">
-            <h2>Wideshow</h2>
+        <div className = "heading">
+        <h1>Explore the world! Enjoy the life!</h1>
+        <h4>Plan a different kind of gateway to uncover the hidden gems near you</h4>
+        <Button variant = "outlined">Explore Nearby</Button>
+        </div>
+           
         </div>
     )
 }
