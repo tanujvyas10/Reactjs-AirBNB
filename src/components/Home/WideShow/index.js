2b2cbd6ed040f320d0256d39b743dbfc9fbcc9f8
tanuj <tanujvyas10@gmail.com>
2020-09-11 14:55:52 +0530

    wideshow addition

---
+++ src/components/Home/WideShow/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './wideshow.css'
+const index = () => {
+    return (
+        <div className = "wideshow">
+            <h2>Wideshow</h2>
+        </div>
+    )
+}
+
+export default index
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import WideShow from './WideShow'
 import './main.css'
 const index = () => {
     return (
         <div className = 'home'>
             <h1>Home Components</h1>
-        </div>
+<WideShow/>
+            </div>
     )
 }
 
