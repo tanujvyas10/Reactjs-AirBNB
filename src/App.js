259e1f63cebf0c3157315369a934de3da0036741
tanuj <tanujvyas10@gmail.com>
2020-09-12 13:14:15 +0530

    searchpage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import Home from './components/Home'
 import Header from './components/Header'
+import SearchPage from './components/SearchPage'
 import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
+    <Router>
     <Header/> 
-    <Home/>
+<Switch>    
+<Route exact path = "/" component ={Home}/>
+<Route exact path = "/search" component ={SearchPage}/>
+
+
+</Switch>
+
+
+
+
     <Footer/> 
-    </div>
+    
+    </Router>
+</div>
   );
 }
 
